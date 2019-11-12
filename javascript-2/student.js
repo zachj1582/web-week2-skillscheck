f491fe0f8391bd43735895442fefc4bb80beffb2
Matias Perez <matias.perezferrero@gmail.com>
2019-11-12 14:11:46 -0700

    edited instructions for clarity

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -1,28 +1,28 @@
 //////////////////SETUP////////////////////
-// Open javascripot-2/index.html in the browser. You should see "An Ode to Code" and a small box below it.
+// Open javascript-2/DO_NOT_EDIT/index.html in the browser. You should see "An Ode to Code" and a small box below it.
 
 //////////////////STEP 1////////////////////
-//Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
+//Create a variable called 'lovesCode' and assign it a value of true or false (as a boolean) depending on if you love code or not. (It's okay if you don't yet!)
 
 // Code Here
 
 //////////////////STEP 2////////////////////
-//Create a variable called age that is your age as a number
+//Create a variable called 'age' that has a value equal to your age as a number. 
 
 // Code Here
 
 //////////////////STEP 3////////////////////
-//Create a variable called name that is your name as a string
+//Create a variable called 'name' and assign it a value of your name as a string.  
 
 // Code Here
 
 //////////////////STEP 4////////////////////
-//Create a variable called phrase that is initially set as an empty string
+//Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
 
 //////////////////STEP 5////////////////////
-//Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
+//Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
 
--- javascript-4/student.js
+++ javascript-4/student.js
@@ -2,46 +2,46 @@
 // Close all other index.html files and open the index.html file in javascript-4/index.html. You should see an input and button that says "Add Item"
 
 //////////////////Step 1////////////////////
-// Create a new array called 'faveColors' and set it your three favorite colors as strings.
+// Create a new array called 'faveColors' and assign it a value of your three favorite colors as strings.
 
 // CODE HERE
 
 //////////////////Step 2////////////////////
-// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
+// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, and profileImage. 
 
-// The firstName key should be your name as a string. 
+// The firstName key should have a value of your name as a string. 
 
-// The superHeroName key should be something fun, feel free to jazz it up e.g. "Bodacious Bryan". 
+// The superHeroName key should be your name if you were a super hero. Feel free to jazz it up and get silly with it, e.g. "Thunderous Gas Man". 
 
-// The homeTown key should be your home town. 
+// The homeTown key value you should be your home town. 
 
-// The superPowers key should be an array of your three super powers as a string. e.g. ['skateboarding', 'crime fighting', 'crime inciting']
+// The superPowers key value should be an array with your three super powers as strings. e.g. ['Mega Blast', 'Thunder From Down Under', 'Calamitous Crop Duster']
 
 //  The superPowerXP key will be a method that returns a random number between 1-100. Use Math.floor(Math.random() * 100) + 1 
 
-// The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
+// The profileImage key should be set to this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
 // CODE HERE
 
 //////////////////Step 3////////////////////
-// Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
+// In this step, you will create three variables that will hold some data from your me object. The first variable you create should be 'regularName', and it should receive its value from the value of your firstName key on the me object above. The next variable is 'superName' which should have its value assigned as the value of the superHeroName property on the me object. The last variable you should create is 'homeTown' which will be the value of homeTown on the me object
 
 // CODE HERE
 
 //////////////////Step 4////////////////////
-//Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors. 
+//Create a function called 'setColor' that takes in one parameter called 'arr' that is an array of colors. Make sure that we only ever have three colors by using splice to trim the array to just 3 colors. 
 
-// Next, create a for loop to loop over the array. If any of the colors you chose is 'blue', change it's value to '#4D4DFF' which is just a more appealing color of blue. Outside of the for loop but still inside of setColor, invoke the function called background which will take in three arguments. These arguments should be each item of your array. This is a function we created for you to set the background colors.
+// Next, create a for loop to loop over the remaining three colors. If any of the colors is 'blue', change it's value to '#4D4DFF' (which is just a more appealing shade of blue). Outside of the for loop but still inside of setColor, invoke the function called 'background' which will take in three arguments. These arguments should be the three items remaining in your colors array. 
 
 // CODE HERE
 
 //////////////////Step 5////////////////////
-//Create a function called 'setPowers' that takes in arr as a parameter. Loop over the arr param and run a function called createLi(), which will take each item of the array as an agument. The createLi function is a function we created to set the data on the screen. It outside the scope of this project
+//Create a function called 'setPowers' that takes in 'arr' as a parameter. In your setPowers function, loop over the arr parameter and run a function we created called createLi(), which will take each item of the array as an argument. Remember, you did not create the createLi function. The createLi function is a function we created that determines how the content is displayed.
 
 // CODE HERE
 
 
 //////////////////Step 6////////////////////
-//Lastly, create a function called redactInfo that will take in an obj as a paramter. Let's imagine our super hero needs to go undercover and we need to remove all info about them. That is what this function will do. Loop over the object and change each keys value to 'redacted'. Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created that will update the text on the screen.
+//Lastly, create a function called 'redactInfo' that will take in an object called 'obj' as a parameter. Now, imagine your super hero needs to go undercover, and you need to remove all info about them. That is what this function will do. In your function, loop over the obj parameter passed in, and change each keys value to 'redacted'. Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created that will update the text on the screen.
 
 // CODE HERE
