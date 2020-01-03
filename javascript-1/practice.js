b97c0c0fb5c9b76005482bdf515fea76acd2d700
Zachary Johnson <zach_j@att.net>
2020-01-03 13:33:38 -0700

    first commit

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,81 +2,120 @@
 
 // Create a variable called 'myName' with a value that is your name. 
 
-// Code here
+let myName = 'Zach'
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
-//Code here
+let faveNum = 14
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
 
-//Code here
+const lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
 
-//Code here
+function sum(num1,num2) {
+	return num1 + num2;
+} 
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
 
-//Code here
+function lovesCodeChecker(x){
+	if(x === true){
+	return true
+} else {
+	return false
+}
+}
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, 'num'. Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'. If the number is even, return 'the number is even'.
 
-//Code here
+function oddChecker(num){
+	if(num % 2 === 0){
+		return 'the number is even'
+	} else {
+		return 'the number is odd'
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. Have the function take the two parameters and return a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
 
-//Code Here
+function iLove(name, love){
+	return `${name} loves ${love}`
+}
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice.
 const faveColors = ['red', 'green', 'black']
 
-//Code Here
+let colorCopy = faveColors.slice()
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push.
 
-//Code Here
+colorCopy.push('purple')
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+let middleNums = numbers.slice(1,4)
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has the following keys: firstName, state, age, and greeter. The value of the firstName key should be your name as a string. The value of the property state should be your current state or providence of residence as a string. The value of age should be your age as a number. greeter should be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the corresponding values. 
 
-//Code Here
+let me = {
+	firstName: 'Zach', 
+	state: 'Texas',
+	age: 37,
+	greeter: function(){
+		return `Hello! My name is ${firstName} and I live in ${state}`
+	} 
+}
 
 //////////////////PROBLEM 12////////////////////
 
 // Create a function called 'bigOrSmall' that takes in one parameter, 'arr', which will be an array of numbers. Inside of the bigOrSmall function, create a new array called 'answers'. Then, loop over the passed in arr parameter, and check to see if the number in the array is GREATER than 100. If it is, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function.
 
-// Code here
+function bigOrSmall(arr){
+	let answers = [];
+	for(let i = 0; i <= arr.length - 1; i++){
+		if(arr[i] > 100){
+			answers.push('big')
+		}else {
+			answers.push('small')
+		}
+	}
+  return answers
+}
 
 //////////////////PROBLEM 13////////////////////
 
 // Create a function called 'arrayReverser' that takes in one parameter, 'arr'. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array IN REVERSE (this means your counter should decrement), and then add each item to the 'reversed' array variable you created. Finally, return the 'reversed' array variable. 
 
-//Code Here
+function arrayReverser(arr){
+	let reversed = []
+	for(let i = arr.length - 1; i >= 0; i--){
+		reversed.push(arr[i])
+	}
+	return reversed;
+}
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
 
@@ -88,32 +127,34 @@ const myNumbers = [3, 55, 788, 2, 1]
 
 // First, use .map to create a new array that doubles each number. Call the new array 'doubled'.
 
-// Code Here
+let doubled = myNumbers.map((v,i,a) => v * 2)
 
 //////////////////PROBLEM 15////////////////////
 
 // Now, use .filter to create a new array containing any numbers that are greater than 100. Call the new array 'filtered'. 
 
-// Code Here
+let filtered = myNumbers.filter(element => element > 100)
 
 //////////////////PROBLEM 16////////////////////
 
 // Next, use reduce to get the total of the numbers in the array summed together. Store the total in a new variable named 'total'. 
 
-// Code Here
+let total = myNumbers.reduce((acc,curr) => acc + curr)
 
 //////////////////PROBLEM 17////////////////////
 
 // Finally, use .forEach to find the index of each item in the array. To begin, create an empty array called 'myNumbersIndex'. Then, use forEach to push each item's index from the myNumbers array into the newly created myNumbersIndex array. 
 
-// Code Here
+let myNumbersIndex = []
+myNumbers.forEach((val,index) => myNumbersIndex.push(index))
+
 
 //////////////////PROBLEM 18////////////////////
 
 // Did you know that George Foreman has five sons named George? Go ahead and change everyone's name in the notGeorge array to George using .map. Store the resulting array in a variable named 'forTheLoveOfGeorge'.
 const notGeorge = ['Louis', 'Ted', 'Bill', 'Sharon', 'Mark', 'Angela']
 
-// Code Here
+let forTheLoveOfGeorge = notGeorge.map((v,i,a) => v = 'George')
 
 //////////////////PROBLEM 19////////////////////
 
@@ -127,10 +168,10 @@ const people = [
 	{ name: 'Josh', friend: true, awesomeLevel: 7 }
 ]
 
-// Code Here
+let enemies = people.filter((element,i,a) => element.friend === false)
 
 //////////////////PROBLEM 20////////////////////
 
 // Using the people array above, get a total of the awesomeLevel from all the people, and store that value in a variable named 'totallyAwesome'. Use .reduce().
 
-// Code Here
+let totallyAwesome = people.reduce((acc,curr) => acc.awesomeLevel + curr.awesomeLevel)
--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,27 +4,31 @@
 //////////////////STEP 1////////////////////
 //Create a variable called 'lovesCode' and assign it a value of true or false (as a boolean) depending on if you love code or not. (It's okay if you don't yet!)
 
-// Code Here
+let lovesCode = true
 
 //////////////////STEP 2////////////////////
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
-// Code Here
+let age = 37
 
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
 
-// Code Here
+let name = 'Zach'
 
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
-// Code Here
+let phrase = ''
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
-// Code Here
+if(lovesCode === true){
+    phrase = 'I love code'
+} else {
+    phrase = 'I do not love to code'
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
